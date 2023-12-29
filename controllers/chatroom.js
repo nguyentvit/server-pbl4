f922e5e15a57015e5122066e945a6bd3a80d3ac8
Nguyên Trần Văn <102210175@sv1.dut.udn.vn>
2023-12-29 08:52:46 +0700

    sdf

--- controllers/chatroom.js
+++ controllers/chatroom.js
@@ -120,7 +120,7 @@ export default {
 
             return res.status(200).json({
                 success: true,
-                roomsInfoWithName
+                rooms
             });
             
 
